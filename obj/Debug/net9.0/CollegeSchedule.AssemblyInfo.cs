7766f72c2e45f7bf9e8863873515b1fa7da6fa76
Vadim <el.soback2001@.mail.ru>
2026-02-09 22:41:13 +0700

    add Specialty class

--- obj/Debug/net9.0/CollegeSchedule.AssemblyInfo.cs
+++ obj/Debug/net9.0/CollegeSchedule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d546a72127e4910818996ddd9ad02c9180c52cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0b5c284e7904bcb5c600580f46505e656adaf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
