270e0e1fc42ba911482d2f0d24642f6c9ebbc161
Vadim <el.soback2001@.mail.ru>
2026-02-09 22:44:54 +0700

    add Teacher class

--- obj/Debug/net9.0/CollegeSchedule.AssemblyInfo.cs
+++ obj/Debug/net9.0/CollegeSchedule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7766f72c2e45f7bf9e8863873515b1fa7da6fa76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb77e0d4261b245800471decb8130f2396d8415f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
