8cbad3308b612a0d2bbe06e828d77c6842969deb
Vadim <el.soback2001@.mail.ru>
2026-02-10 22:25:39 +0700

    update AppDbContext class

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,9 +5,8 @@ namespace CollegeSchedule.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+    }
         public DbSet<Building> Buildings => Set<Building>();
         public DbSet<Classroom> Classrooms => Set<Classroom>();
         public DbSet<Teacher> Teachers => Set<Teacher>();
