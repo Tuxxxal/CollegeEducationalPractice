2fc0077ec6635c36149402a5442c7b8afd333583
Vadim <el.soback2001@.mail.ru>
2026-02-10 23:34:39 +0700

    add ScheduleController class

---
+++ Controllers/ScheduleController.cs
@@ -0,0 +1,37 @@
+﻿using CollegeSchedule.Data;
+using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+
+        // Убираем AppDbContext из конструктора — он не нужен здесь!
+        public ScheduleController(IScheduleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            try
+            {
+                var result = await _service.GetScheduleForGroup(groupName, start, end);
+                return Ok(result);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
