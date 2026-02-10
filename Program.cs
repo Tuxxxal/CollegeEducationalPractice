ad4137b26e35927ceb80bd7b9ce5c310c65d4cfe
Vadim <el.soback2001@.mail.ru>
2026-02-10 23:18:51 +0700

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -9,12 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 DotNetEnv.Env.Load();
 var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
-$"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
-$"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
-$"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
-$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+                       $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                       $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                       $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseNpgsql(connectionString));
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
